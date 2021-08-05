4fd77a50e19cc4a67959a058d9bb33463b3c42b7
lucas <lucasbastossp@hotmail.comm>
2021-08-05 16:10:35 -0300

    consulta nick

--- nick.py
+++ nick.py
@@ -1,3 +1,6 @@
+import json
+import requests
+
 print(15*'----')
 
 nick = str(input('Digite seu Nick: '))
@@ -7,3 +10,10 @@ print(f'Seu nick é {nick}')
 #api = resultado do arquivo
 
 #http://riot.api.com/+'api'
+
+api_token = 'RGAPI-310934e4-9f2c-4911-ae2b-8f8dd57c8d50'
+url_nicks = 'https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+nick+'?api_key='+api_token
+
+r = requests.get(url_nicks)
+todos = json.loads(r.content)
+print(todos)
