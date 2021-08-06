d136f16c404f53a3a65d39bfc1015e9c854bac1d
lucas <lucasbastossp@hotmail.comm>
2021-08-05 23:51:04 -0300

    envio de msg via Whatsapp

---
+++ EnvioMSGWhatssapp.py
@@ -0,0 +1,7 @@
+import pywhatkit as whatsapp
+
+whatsapp.sendwhatmsg('+5511948817436','Queria apenas lembrar o quanto eu te amo e sou louco por voce!!!!',23,45)
+whatsapp.sendwhatmsg('+5511948817436','Queria apenas lembrar o quanto eu te amo e sou louco por voce!!!!',23,48)
+whatsapp.sendwhatmsg('+5511948817436','Queria apenas lembrar o quanto eu te amo e sou louco por voce!!!!',23,49)
+whatsapp.sendwhatmsg('+5511948817436','Queria apenas lembrar o quanto eu te amo e sou louco por voce!!!!',23,50)
+
