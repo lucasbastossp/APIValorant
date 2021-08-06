a79d6913fba6a71ca4a6bcd723d3716ea3c545dd
lucas <lucasbastossp@hotmail.comm>
2021-08-05 22:59:43 -0300

    buscar de champions.... inicio

--- nick.py
+++ nick.py
@@ -1,56 +1,66 @@
 import json
 import requests
+from urllib.request import urlopen
 
-print(15*'----')
-
-nick = str(input('Digite seu Nick: '))
-print(f'Seu nick é {nick}')
-
-#ler o arquivo.txt
-#api = resultado do arquivo
-
-#http://riot.api.com/+'api'
+print(20*'----')
 
 arq = open('config.txt')
 linhas = arq.readlines()
 for linha in linhas:
     print(linha)
 
+
+
+nick = str(input('Digite seu nick: '))
+
+
 url_nicks = 'https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+nick+'?api_key='+linha
 
 r = requests.get(url_nicks)
 todos = json.loads(r.content)
 
+print('Seu nick é ' + todos['name'])
+print('Seu level é ' + str(todos['summonerLevel']))
+#print(type(todos))
 
+#print(todos)
 
-'''' Fazer ficar perguntando o nick ate o usuario informar que deseja sair
-##### e sempre exibir apos o usuario digitiar seu nick
-######## Guardar accountID nick e lvl
-#exibir somente o Nick e LVL '''
-
-
-print(type(todos))
-
-print(todos)
-
-
-
-
-print('Deseja fazer outra escolha? \n [1] Sim \n [2] Não ')
 
 contador = 0
-while contador == 0:
-    escolha = int(input('Digite 1 ou 2: '))
-    if escolha == 1:
 
-        print('Digite outro Nick: ')
-        arq= open('config.txt')
-        linhas = arq.readlines()
-        for linha in linhas:
-            print(linha)
-        url_nicks = 'https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+nick+'?api_key='+linha
+def buscarDadosPlayer(nick):
+    url_nicks = 'https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+nick+'?api_key='+linha
+    r = requests.get(url_nicks)
+    todos = json.loads(r.content)
+    print(todos)
+    print('Seu nick é ' + todos['name'])
+    name = str(todos['name'])
+    print('Seu level é ' + str(todos['summonerLevel']))
+    lvl = str(todos['summonerLevel'])
+    acc_id = str(todos['accountId'])
 
-        r = requests.get(url_nicks)
-        todos = json.loads(r.content)
-        print(type(todos))
-        print(todos)
+def buscarHerois():
+    champions = 'http://ddragon.leagueoflegends.com/cdn/11.15.1/data/en_US/champion.json'
+    response = urlopen(champions)
+    data_json = json.loads(response.read())
+    print(type(data_json))
+    #print(data_json)
+    print(type(champions))
+    print(champions)
+    #for n in data_json['data']:
+    #    print(n)
+
+while (contador < 999):
+    resultado = str(input('Deseja consultar outro Nick? (S/N) ')).strip() .upper()[0]
+    if resultado == 'S':
+        nick = str(input('Digite outro nick: '))
+
+        buscarDadosPlayer(nick)
+
+    else:
+        print('Programa encerrado!')
+        break
+
+
+
+buscarHerois()
